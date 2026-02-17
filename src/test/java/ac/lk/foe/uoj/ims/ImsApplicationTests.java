48ee718c0f505522da7763b127931e829471e3ec
sachinthanimesh370 <sachinthanimesh370@gmail.com>
2026-02-17 10:48:14 +0530

    Initialied project template

---
+++ src/test/java/ac/lk/foe/uoj/ims/ImsApplicationTests.java
@@ -0,0 +1,13 @@
+package ac.lk.foe.uoj.ims;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
