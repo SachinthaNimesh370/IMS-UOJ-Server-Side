b6a51f262c1380bd66f8321783d7acaa0f591bc2
sachinthanimesh370 <sachinthanimesh370@gmail.com>
2026-03-09 19:41:28 +0530

    Create Department entity

--- src/main/java/ac/lk/foe/uoj/ims/entity/Department.java
+++ src/main/java/ac/lk/foe/uoj/ims/entity/Department.java
@@ -1,4 +1,23 @@
 package ac.lk.foe.uoj.ims.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String code;
+    private boolean status;
 }
